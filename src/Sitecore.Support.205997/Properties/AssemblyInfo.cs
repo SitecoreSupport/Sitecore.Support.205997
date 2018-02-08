64412e5da6001b329bdd5df29a99fbe5cb7a9410
Pavel <piv@sitecore.net>
2018-02-08 14:11:52 +0200

    Fix issue #205997 by creating a rule according toxConnect API

---
+++ src/Sitecore.Support.205997/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.205997")]
+[assembly: AssemblyProduct("Sitecore.Support.205997")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.205997/XConnect/Segmentation/Predicates/Interactions/PageOrDescendantsVisitMatches.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Sitecore.Framework.Rules;
+using Sitecore.XConnect;
+using Sitecore.XConnect.Collection.Model;
+
+namespace Sitecore.Support.XConnect.Segmentation.Predicates.Interactions
+{
+  public class PageOrDescendantsVisitMatches : ICondition
+  {
+    public Guid ItemId { get; set; }
+
+    [SuppressMessage("Data Flow", "SC1062:ValidateArgumentsOfPublicMethods", MessageId = "0#")]
+    public bool Evaluate(IRuleExecutionContext context)
+    {
+      var contact = context.Fact<Contact>();
+      if (contact.Interactions == null) return false;
+      return contact.Interactions.Any(i => i.Events.OfType<PageViewEvent>().Any(e => e.ItemId == this.ItemId)) || contact.Interactions.SelectMany(i => i.Events.OfType<PageViewEvent>()).Select(i => i.ItemId).Any(CheckItem);
+    }
+
+    private bool CheckItem(Guid id)
+    {
+      var item = Configuration.Factory.GetDatabase("master").GetItem(id.ToString());
+      var parent = item?.Parent;
+
+      if (parent == null) return false;
+      while (parent != null)
+      {
+        if (parent.ID.ToGuid() == this.ItemId)
+        {
+          return true;
+        }
+        parent = parent.Parent;
+      }
+      return false;
+    }
+  }
+}
